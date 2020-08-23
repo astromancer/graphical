216814c07748d97a4869c2590f0e4f25fbb3f1fa
astromancer <hannes@saao.ac.za>
2020-08-23 20:50:37 +0200

    update install script

--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
-
 import site
 import inspect
 import subprocess
 from pathlib import Path
 
 
-def link_into_path():
+def link_install_cheat():
     """
-    Simple install strategy that links the repo's man directory into system
-    python path.  Not sure if this will always work...
+    Cheat install that links the repo's main directory into system python
+    path.  Good for development libraries - changes to the code base are
+    immediately available to the interpreter :)
+
+    Can be used as is for any simple python library
     """
     here = inspect.getfile(inspect.currentframe())
     here = Path(here)
     src = here.parent.resolve()
+
     pkg_name = src.name
-    src = str(src)
-    dest = site.getsitepackages()[0]
+    src = str(src / src.name)
+    dest = site.getusersitepackages()
     dest = str(Path(dest) / pkg_name)
 
-    # print(src, dest)
-    subprocess.call(['ln', '-s', src, dest])
+    print('linking', src, dest)
+    ok = subprocess.call(['ln', '-sf', src, dest])
     # ln -s path/to/repo/eeg `python3 -m site --user-site`/eeg
+    if ok == 0:
+    	print('Installed', pkg_name, 'at', dest, 'via cheat')
+    return ok
 
-
-link_into_path()
+happiness = link_install_cheat()
