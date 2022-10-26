19508c8be8f3e37f93df3a87c0e6884aa6e321b9
astromancer <hannes@saao.ac.za>
2022-10-26 21:05:27 +0200

    DEV: add class for tracking axes under cursor

--- src/scrawl/moves/__init__.py
+++ src/scrawl/moves/__init__.py
@@ -4,3 +4,17 @@ Movable artists.
 
 from .errorbars import *
 from .callbacks import *
+
+
+class TrackAxesUnderMouse(CallbackManager):
+    _axes_under_mouse = None
+
+    @mpl_connect('axes_enter_event')
+    def _on_enter(self, event):
+        self.logger.debug('Mouse over: {}', event.inaxes)
+        self._axes_under_mouse = event.inaxes
+
+    @mpl_connect('axes_leave_event')
+    def _on_leave(self, event):
+        self.logger.debug('Mouse left axes: {}', event.inaxes)
+        self._axes_under_mouse = None
